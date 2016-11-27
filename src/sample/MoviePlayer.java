7235abbf81d648cd2b81f509cd330970b9aa3b5a
N7 <n7russia@gmail.com>
2016-11-28 01:42:01 +0500

    init

--- src/sample/MoviePlayer.java
+++ src/sample/MoviePlayer.java
@@ -1,4 +1,7 @@
 package sample;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -7,11 +10,14 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.media.AudioSpectrumListener;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -31,9 +37,34 @@ public class MoviePlayer extends Application {
         final MediaPlayer player = new MediaPlayer(media);
         MediaView view = new MediaView(player);
         //System.out.println("media.wigth: " +media.getWidh());
+        final Timeline slideIn= new Timeline();
+        final Timeline slideOut= new Timeline();
+        root.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseevent) {
+                slideOut.play();
+            }
+        });
+        root.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseevent) {
+                slideIn.play();
+            }
+        });
+
         final VBox vbox = new VBox();
-        Slider slider = new Slider();
+        final Slider slider = new Slider();
         vbox.getChildren().add(slider);
+
+        final HBox hbox = new HBox(2);
+        final int bands = player.getAudioSpectrumNumBands();
+        final Rectangle[] rects= new Rectangle[bands];
+        for (int i=0; i<rects.length; i++){
+            rects[i] = new Rectangle();
+            rects[i].setFill(Color.GREENYELLOW);
+            hbox.getChildren().add(rects[i]);
+        }
+        vbox.getChildren().add(hbox);
 
         root.getChildren().add(view);
         root.getChildren().add(vbox);
@@ -49,14 +80,45 @@ public class MoviePlayer extends Application {
             public void run() {
                 int w = player.getMedia().getWidth();
                 int h = player.getMedia().getHeight();
+
+                hbox.setMinWidth(w);
+                int bandWidth=w/rects.length;
+                for(Rectangle r:rects){
+                    r.setWidth(bandWidth);
+                    r.setHeight(2);
+                }
                 primaryStage.setMinWidth(w);
                 primaryStage.setMinHeight(h);
 
                 vbox.setMinSize(w, 100);
                 vbox.setTranslateY(h-100);
+
                 slider.setMin(0.0);
                 slider.setValue(0.0);
                 slider.setMax(player.getTotalDuration().toSeconds());
+                slideOut.getKeyFrames().addAll(
+                        new KeyFrame(new Duration(0),
+                                new KeyValue(vbox.translateYProperty(), h-100),
+                                new KeyValue(vbox.opacityProperty(), 0.9)
+                        ),
+                        new KeyFrame(new Duration(300),
+
+                                new KeyValue(vbox.translateYProperty(), h),
+                                new KeyValue(vbox.opacityProperty(), 0.0)
+                        )
+
+                );
+                slideIn.getKeyFrames().addAll(
+                        new KeyFrame(new Duration(0),
+                                new KeyValue(vbox.translateYProperty(), h),
+                                new KeyValue(vbox.opacityProperty(), 0.0)
+                        ),
+                        new KeyFrame(new Duration(300),
+                                new KeyValue(vbox.translateYProperty(), h-100),
+                                new KeyValue(vbox.opacityProperty(), 0.9)
+                        )
+
+                );
             }
 
         });
@@ -72,7 +134,17 @@ public class MoviePlayer extends Application {
                 player.seek(Duration.seconds(slider.getValue()));
             }
         });
-        // controller
+        player.setAudioSpectrumListener(new AudioSpectrumListener() {
+            @Override
+            public void spectrumDataUpdate(double v, double v1, float[] mags, float[] floats1) {
+                for (int i=0; i<rects.length; i++){
+                    double h = mags[i]+60;
+                    if (h>2) {
+                        rects[i].setHeight(h);
+                    }
+                }
+            }
+        });
     }
 }
 
